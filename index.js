328f6d318e3fd127d27650f1e3cb26b1247bf624
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-03-21 19:27:51 -0300

    pure vs impure function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hi');
+// pure function 
+function priceAfterTax(productPrice) {
+    return (productPrice * 1.20)
+   }
+
+console.log(priceAfterTax(20));
+
+// impure function
+// it the tax is updated, the function's result will change.
+var tax = 20;
+function calculateTax(productPrice) {
+ return (productPrice * (tax/100)) + productPrice; 
+}
+
+console.log(calculateTax(tax));

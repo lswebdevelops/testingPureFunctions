1b93c7cc6f12aa54fa3abcc0e195c8106a366cf3
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-03-23 20:54:58 -0300

    testing chunks of 2 and 3

--- chunk.test.js
+++ chunk.test.js
@@ -5,9 +5,20 @@ test('ChunkArray function exists', ()=>{
     expect(chunkArray).toBeDefined();
 });
 
+test('chunk an array of 10 values with length of 3',()=>{
+    const numbers = [1,2,3,4,5,6,7,8,9,10];
+    let len = 3;
+    const chunkedArr = chunkArray(numbers, len);
+    expect(chunkedArr).toEqual([[1,2,3],[4,5,6], [7,8,9],[10]]);
+    
+});
+
 test('chunk an array of 10 values with length of 2',()=>{
     const numbers = [1,2,3,4,5,6,7,8,9,10];
-    const len = 2;
+    
+    len = 2;
     const chunkedArr = chunkArray(numbers, len);
     expect(chunkedArr).toEqual([[1,2],[3,4],[5,6], [7,8],[9, 10]]);
+
+
 });

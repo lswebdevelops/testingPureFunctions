5c29aca602242a4b4cd30fcb281bfd5456e0982f
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-03-22 21:09:13 -0300

    using regex

--- functions.test.js
+++ functions.test.js
@@ -36,4 +36,24 @@ test('User should be Brad Traversy object',()=>{
 })
 
 
+// less than and greater than 
+
+test('Should be under 1600', ()=>{
+    const load1 = 800;
+    const load2 = 700;
+    expect (load1 + load2).toBeLessThan(1600)
+    expect(load1 + load2).toBeLessThanOrEqual(1600)
+    expect(load1 + load2).toBeGreaterThanOrEqual(1500)
+
+})
+
+// regex
+
+test('There is no I in team', () =>{
+    expect('team').not.toMatch(/I/);
+    expect('tIam').toMatch(/I/);
+
+})
+//arrays
+
 
